8725b5ce679a9632045c890a3ec1cc7924a21755
Kelvin Puch <kelpucha@gmail.com>
2025-09-07 16:42:41 -0400

    upd

--- guessing-num.py
+++ guessing-num.py
@@ -1,8 +1,7 @@
 import random
 
+
 def guessing_game():
-
-
     number_to_guess = random.randint(1, 100)
     attempts = 0
     max_attempts = 10
@@ -25,12 +24,16 @@ def guessing_game():
             elif guess > number_to_guess:
                 print("Too high!")
             else:
-                print(f"Congratulations! You've guessed the number {number_to_guess} in {attempts} attempts.")
+                print(
+                    f"Congratulations! You've guessed the number {number_to_guess} in {attempts} attempts."
+                      )
                 break
         except ValueError:
             print("Invalid input. Please enter a number.")
-
     if attempts == max_attempts and guess != number_to_guess:
-        print(f"Sorry, you've used all your attempts. The number was {number_to_guess}.")
+        print(
+            f"Sorry, you've used all your attempts. The number was {number_to_guess}."
+        )
+
 
 guessing_game()
